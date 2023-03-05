36417d9e122496ee56fef4697a8c682e017df545
Nandan Kumar Singh <nandan3512@gmail.com>
2023-03-06 02:00:22 +0530

    price filter added

--- src/Components/Jewellery/Filter.jsx
+++ src/Components/Jewellery/Filter.jsx
@@ -3,11 +3,13 @@ import {
   Center,
   Checkbox,
   Divider,
+  Radio,
+  RadioGroup,
   Stack,
   Text,
 } from "@chakra-ui/react";
 
-export const Filter = ({ onFilterChange, categories }) => {
+export const Filter = ({ onFilterChange, categories, onFilterPrice, prices }) => {
   return (
     <Box p={5} border="1px solid black" borderRadius={30} bgColor={"#f6f3f9"} position='sticky' top={'20%'}>
       <Center h={"50px"} fontSize="lg">
@@ -23,9 +25,12 @@ export const Filter = ({ onFilterChange, categories }) => {
         }}
       >
         <Text as={"b"}>Price</Text>
-        <Checkbox>Under ₹50,000</Checkbox>
-        <Checkbox>₹50,000 - ₹1,00,000</Checkbox>
-        <Checkbox>Over ₹1,00,000</Checkbox>
+        {prices.map((price) => (
+          <label key={price}>
+            <input onChange={onFilterPrice} type="checkbox" value={price} />
+            {price}
+          </label>
+        ))}
       </Stack>
       <Divider p={1} />
       <Stack
