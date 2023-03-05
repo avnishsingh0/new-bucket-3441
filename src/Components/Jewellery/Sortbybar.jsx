1d1154b2d5d695d3bd78cd0f646744cd4fef7765
Nandan Kumar Singh <nandan3512@gmail.com>
2023-03-06 02:03:22 +0530

    price filter added

--- src/Components/Jewellery/Sortbybar.jsx
+++ src/Components/Jewellery/Sortbybar.jsx
@@ -24,16 +24,6 @@ export const Sortbybar = ({ sortby }) => {
 
         <Flex w={"40%"} bgGradient="linear(to-r, #fcd5f5, #bfb3fe)">
           <Spacer />
-          <Select
-            w={"40%"}
-            p={7}
-            bg
-            placeholder="Sort By:"
-            onClick={(e) => sorthandleclick(e)}
-          >
-            <option value="option1">Price: Low to High</option>
-            <option value="option2">Price: High to Low</option>
-          </Select>
         </Flex>
       </Flex>
     </Box>
